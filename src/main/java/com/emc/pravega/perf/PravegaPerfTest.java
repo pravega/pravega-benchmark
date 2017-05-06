34a68d6c70b51e307d27b65d1aa3d39e16bc2667
arvindkandhare <arvind.kandhare@emc.com>
2017-05-05 22:07:46 -0700

    Ensure that checkpoints is initialized properly

--- src/main/java/com/emc/pravega/perf/PravegaPerfTest.java
+++ src/main/java/com/emc/pravega/perf/PravegaPerfTest.java
@@ -129,6 +129,7 @@ public class PravegaPerfTest {
                     reader.cleanupEvents();
                 executor.execute(reader);
             }
+            if(consumerCount == 0)
             readerGroup.initiateCheckpoint(streamName, executor);
         }
         produceStats = new PerfStats("Writing",producerCount * eventsPerSec * runtimeSec, reportingInterval,
