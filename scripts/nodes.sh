0fd548dcbd3ac586c56f2d6effa37756fdd83c2b
Keshava Munegowda <keshava.munegowda@dell.com>
2019-03-13 16:32:29 +0530

    Added license header to config.properties and script files
    Signed-off-by: Keshava Munegowda <keshava.munegowda@dell.com>

--- scripts/nodes.sh
+++ scripts/nodes.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
 
 # replace with appropriate IP addresses
 export NODES="10.240.135.13 10.240.135.20 10.240.135.8"
