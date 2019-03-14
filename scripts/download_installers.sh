1d90a1ffbcd85370d6aa280fa7fb69c06218a1e3
Keshava Munegowda <keshava.gowda@gmail.com>
2019-03-14 16:49:01 +0530

    Issue 26: Replace license headers with the ones used in Pravega (#35)
    Replaced license headers with the ones used in Pravega.
    
    Signed-off-by: Keshava Munegowda <keshava.munegowda@dell.com>

--- scripts/download_installers.sh
+++ scripts/download_installers.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+
 set -e
 set -x
 wget -c http://www.apache.org/dist/hadoop/core/hadoop-2.7.3/hadoop-2.7.3.tar.gz -o ./cache/hadoop-2.7.3.tar.gz
--- scripts/install_pravega.sh
+++ scripts/install_pravega.sh
@@ -1,4 +1,13 @@
-
+#!/usr/bin/env bash
+#
+# Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
 . ./scripts/nodes.sh
 scp cache/pravega-controller-0.1.0-SNAPSHOT.tar root@$CONTROLLER_NODE:/root/
 ssh root@$CONTROLLER_NODE tar xvf ./pravega-controller-0.1.0-SNAPSHOT.tar
