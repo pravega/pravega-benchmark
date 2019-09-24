e0b34ec7b318bed0b7bc6b2bf4a559f37a81d5b8
Claudio Fahey <claudio.fahey@dell.com>
2019-09-23 17:23:44 -0700

    Updated PravegaTransactionWriterWorker to use the new Pravega transactional writer API. It now correctly implements writeData and flush methods.
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

--- src/main/java/io/pravega/perf/PravegaTransactionWriterWorker.java
+++ src/main/java/io/pravega/perf/PravegaTransactionWriterWorker.java
@@ -11,22 +11,30 @@
 package io.pravega.perf;
 
 import io.pravega.client.EventStreamClientFactory;
+import io.pravega.client.stream.EventWriterConfig;
 import io.pravega.client.stream.Transaction;
+import io.pravega.client.stream.TransactionalEventStreamWriter;
 import io.pravega.client.stream.TxnFailedException;
+import io.pravega.client.stream.impl.ByteArraySerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.concurrent.GuardedBy;
+import java.util.UUID;
 
-public class PravegaTransactionWriterWorker extends PravegaWriterWorker {
+public class PravegaTransactionWriterWorker extends WriterWorker {
     private static Logger log = LoggerFactory.getLogger(PravegaTransactionWriterWorker.class);
 
+    private static TriConsumer noOpTriConsumer = (a, b, c) -> {};
+
+    private final TransactionalEventStreamWriter<byte[]> producer;
     private final int transactionsPerCommit;
     private final boolean enableWatermark;
 
     @GuardedBy("this")
     private int eventCount;
 
+    // If null, a transaction has not been started.
     @GuardedBy("this")
     private Transaction<byte[]> transaction;
 
@@ -40,22 +48,42 @@ public class PravegaTransactionWriterWorker extends PravegaWriterWorker {
                                    PerfStats stats, String streamName, int eventsPerSec, boolean writeAndRead,
                                    EventStreamClientFactory factory, int transactionsPerCommit, boolean enableConnectionPooling,
                                    boolean enableWatermark) {
-        super(sensorId, events, Integer.MAX_VALUE, secondsToRun, isRandomKey,
-                messageSize, start, stats, streamName, eventsPerSec, writeAndRead, factory, enableConnectionPooling,
-                -1);
+        super(sensorId, events, Integer.MAX_VALUE,
+                secondsToRun, isRandomKey, messageSize, start,
+                stats, streamName, eventsPerSec, writeAndRead);
 
+        final String writerId = UUID.randomUUID().toString();
+        this.producer = factory.createTransactionalEventWriter(
+                writerId,
+                streamName,
+                new ByteArraySerializer(),
+                EventWriterConfig.builder()
+                        .enableConnectionPooling(enableConnectionPooling)
+                        .build());
         this.transactionsPerCommit = transactionsPerCommit;
         this.enableWatermark = enableWatermark;
+
         eventCount = 0;
-        transaction = producer.beginTxn();
     }
 
+    /**
+     * Writes an event in a transaction. It will begin a new transaction if needed.
+     * It periodically commits the current transaction.
+     * Called directly by write-only tests.
+     *
+     * @param data   data to write
+     * @param record to call to record statistics
+     * @return the current time
+     */
     @Override
     public long recordWrite(byte[] data, TriConsumer record) {
         long time = 0;
         try {
             synchronized (this) {
                 time = System.currentTimeMillis();
+                if (transaction == null) {
+                    transaction = producer.beginTxn();
+                }
                 transaction.writeEvent(data);
                 record.accept(time, System.currentTimeMillis(), messageSize);
                 eventCount++;
@@ -67,12 +95,53 @@ public class PravegaTransactionWriterWorker extends PravegaWriterWorker {
                     } else {
                         transaction.commit();
                     }
-                    transaction = producer.beginTxn();
+                    transaction = null;
                 }
             }
         } catch (TxnFailedException e) {
             throw new RuntimeException("Transaction Write data failed ", e);
         }
         return time;
+    }
+
+    /**
+     * Used only by read-write tests.
+     * @param data data to write
+     */
+    @Override
+    public void writeData(byte[] data) {
+        recordWrite(data, noOpTriConsumer);
+    }
+
+    /**
+     * Commits the current transaction if it exists.
+     */
+    @Override
+    public void flush() {
+        try {
+            synchronized (this) {
+                if (transaction != null) {
+                    if (enableWatermark) {
+                        long time = System.currentTimeMillis();
+                        log.info("flush: commit({})", time);
+                        transaction.commit(time);
+                    } else {
+                        transaction.commit();
+                    }
+                    transaction = null;
+                }
+            }
+        } catch (TxnFailedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public synchronized void close() {
+        if (transaction != null) {
+            transaction.abort();
+            transaction = null;
+        }
+        producer.close();
     }
 }
